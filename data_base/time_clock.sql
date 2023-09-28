992b664898094f2d9acc2c010591b19a6f4335e1
ZionAmar <zion0549774827@hotmail.com>
2023-09-28 19:39:07 +0300

    סיום הפרוייקט רק כדי להיות בטוח שהם מעודכנים העלתי את הטבלאות שוב

--- data_base/time_clock.sql
+++ data_base/time_clock.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 27, 2023 at 10:51 PM
+-- Generation Time: Sep 28, 2023 at 06:35 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -36,15 +36,6 @@ CREATE TABLE `time_clock` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `time_clock`
---
-
-INSERT INTO `time_clock` (`id`, `name`, `date`, `entry_time`, `exit_time`) VALUES
-(37, 'ציון עמר', '2023-09-07', '23:47:00', NULL),
-(40, 'ציון עמר', '2023-09-12', '23:21:00', '23:21:00'),
-(45, 'zion amar', '2023-09-14', '00:32:00', '23:32:00');
-
---
 -- Indexes for dumped tables
 --
 
@@ -62,7 +53,7 @@ ALTER TABLE `time_clock`
 -- AUTO_INCREMENT for table `time_clock`
 --
 ALTER TABLE `time_clock`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=183;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
